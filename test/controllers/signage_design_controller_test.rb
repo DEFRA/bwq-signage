3ec73fc4b927b0101e6be9d046f5c71b0279d1e9
Ian Dickinson <i.j.dickinson@gmail.com>
2018-01-26 18:05:07 +0000

    Add some basic content to the landing page to orient the user and start the process. #20

--- test/controllers/signage_design_controller_test.rb
+++ test/controllers/signage_design_controller_test.rb
@@ -11,4 +11,23 @@ class SignageDesignControllerTest < ActionDispatch::IntegrationTest
     find('.header-proposition')
       .must_have_content('Create a bathing water sign')
   end
+
+  it 'should have a meaningful heading' do
+    visit(root_path)
+    find('h1')
+      .must_have_content('Create a bathing water sign')
+  end
+
+  it 'should have some instructions to the user' do
+    visit(root_path)
+    find('.lede')
+      .text
+      .wont_be_empty
+  end
+
+  it 'should have a button to start the design process' do
+    visit(root_path)
+    find('.button-start')
+      .must_have_content('Start now')
+  end
 end
