5160b03f94fcb4b719500f97312b12451f9abd37
Ian Dickinson <i.j.dickinson@gmail.com>
2018-01-26 11:44:48 +0000

    Set the proposition title on the page

--- test/controllers/signage_design_controller_test.rb
+++ test/controllers/signage_design_controller_test.rb
@@ -1,7 +1,14 @@
+# frozen-string-literal: true
+
 require 'test_helper'
 
 class SignageDesignControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Capybara::DSL
+  include Capybara::Minitest::Assertions
+
+  it 'should set the proposition title correctly' do
+    visit(root_path)
+    find('.header-proposition')
+      .must_have_content('Create a bathing water sign')
+  end
 end
