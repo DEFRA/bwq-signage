6ec90b9f2c6042a2a5dba98e3628b14e0028e6fb
Ian Dickinson <i.j.dickinson@gmail.com>
2018-01-31 23:03:48 +0000

    Create a finite state machine for processing through the collection of user-specifiable parameters in stages. We group collections of parameters, such as the choice of the bathing water, and link each collection to a named step. Each step is only complete once the url params contains value for all of the parameters named in that step. Two terminal states begin and end the sequence: :landing and :preview.
    To facilitate jumping back in the sequence to edit parameters, the
    natural step can be overridden by specifying the :step parameter.
    
    Issue #24.

---
+++ test/presenters/workflow_test.rb
@@ -0,0 +1,61 @@
+# frozen-string-literal: true
+
+# Unit tests on Workflow presenter
+class WorkflowTest < ActiveSupport::TestCase
+  describe 'Workflow' do
+    describe '#nominated_step' do
+      it 'should recognise a nominated step' do
+        Workflow.nominated_step(step: 'landing').must_equal :landing
+        Workflow.nominated_step(step: :landing).must_equal :landing
+      end
+
+      it 'should reject a nominated step with an incorrect name' do
+        refute Workflow.nominated_step(step: 'stairs')
+      end
+
+      it 'should not recognise a step with an incorrect key' do
+        refute Workflow.nominated_step(pest: 'landing')
+      end
+    end
+
+    describe '#next_incomplete_step' do
+      it 'should select the landing step if given no information' do
+        Workflow.next_incomplete_step({}).must_equal :landing
+      end
+
+      it 'should select the search step when the design workflow starts' do
+        Workflow.next_incomplete_step(design: true).must_equal :search
+      end
+
+      it 'should select the select step when a search term is given' do
+        Workflow.next_incomplete_step(design: true, search: 'foo').must_equal :select
+      end
+
+      it 'should select the bwmgr step when the bw is selected' do
+        Workflow.next_incomplete_step(design: true, eubwid: '123').must_equal :bwmgr
+      end
+
+      it 'should select the bwmgr step unless all bw manager info is known' do
+        Workflow.next_incomplete_step(design: true, eubwid: '123', 'bwmgr-name': 'foo')
+                .must_equal :bwmgr
+        Workflow.next_incomplete_step(design: true, eubwid: '123', 'bwmgr-email': 'foo',
+                                      'bwmgr-name': 'bar')
+                .must_equal :bwmgr
+      end
+
+      it 'should select options when the bw manager information is complete' do
+        Workflow.next_incomplete_step(design: true, eubwid: '123', 'bwmgr-email': 'foo',
+                                      'bwmgr-name': 'bar', 'bwmgr-phone': '')
+                .must_equal :opts
+      end
+
+      it 'should select preview when all information is complete' do
+        Workflow.next_incomplete_step(design: true, eubwid: '123', 'bwmgr-email': 'foo',
+                                      'bwmgr-name': 'bar', 'bwmgr-phone': '',
+                                      'no-hist': true, 'no-map': false, 'no-prf': true,
+                                      'no-logo': false)
+                .must_equal :preview
+      end
+    end
+  end
+end
