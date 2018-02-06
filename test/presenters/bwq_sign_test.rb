d89c2ecc33213841efeb2c004988909fc1f7ad50
Ian Dickinson <i.j.dickinson@gmail.com>
2018-02-06 09:40:05 +0000

    Add tests for search capability in bwq_sign presenter

--- test/presenters/bwq_sign_test.rb
+++ test/presenters/bwq_sign_test.rb
@@ -13,7 +13,9 @@ class BwqSignTest < ActiveSupport::TestCase
         bwq_sign.params.must_equal mock_params
       end
     end
+  end
 
+  describe 'workflow' do
     describe '#next_workflow_step' do
       it 'should calculate the next workflow step given the parameters' do
         params = ActionController::Parameters.new(design: true).permit!
@@ -21,6 +23,23 @@ class BwqSignTest < ActiveSupport::TestCase
 
         bwq_sign.next_workflow_step.must_equal :search
       end
+    end
+  end
+
+  describe 'search' do
+    it 'should return the search term from the params' do
+      params = ActionController::Parameters.new(search: 'kermit').permit!
+      bwq_sign = BwqSign.new(params: params)
+      bwq_sign.search_term.must_equal('kermit')
+    end
+
+    it 'should delegate searching to the search service' do
+      search_service = mock('search_service')
+      search_service.expects(:search).with('kermit').returns(['miss piggy'])
+
+      params = ActionController::Parameters.new(search: 'kermit').permit!
+      bwq_sign = BwqSign.new(params: params, search: search_service)
+      bwq_sign.search_results.must_equal(['miss piggy'])
     end
   end
 end
