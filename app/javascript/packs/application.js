ebf1e69f9c174ee3c3a655ceb04727bd8ea9d301
Ian Dickinson <i.j.dickinson@gmail.com>
2018-02-16 08:59:42 +0000

    Still attempting to debug in prod

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,7 @@ import SamplingPointMap from '../components/sampling-point-map.vue';
 // wait until the DOM is rendered before we initialise Vue
 document.addEventListener('DOMContentLoaded', () => {
   console.log('In domContentLoaded event handler', process.env.NODE_ENV === 'production');
+  debugger;
   /* eslint-disable no-new */
   new Vue({
     el: '#map-container',
