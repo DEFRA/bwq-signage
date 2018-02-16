11d2b35e94bab3199b92bcea1b7e15b58d540af0
Ian Dickinson <i.j.dickinson@gmail.com>
2018-02-16 08:49:29 +0000

    Temporarily disable Sentry

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,13 +4,13 @@ import Raven from 'raven-js';
 import RavenVue from 'raven-js/plugins/vue';
 import SamplingPointMap from '../components/sampling-point-map.vue';
 
-if (process.env.NODE_ENV === 'production') {
-  // Sentry.io logging - captures run-time error messages
-  Raven
-    .config('https://a893b64b570a4af38a6d410c935a0bea@sentry.io/281446')
-    .addPlugin(RavenVue, Vue)
-    .install();
-}
+// if (process.env.NODE_ENV === 'production') {
+//   // Sentry.io logging - captures run-time error messages
+//   Raven
+//     .config('https://a893b64b570a4af38a6d410c935a0bea@sentry.io/281446')
+//     .addPlugin(RavenVue, Vue)
+//     .install();
+// }
 
 // wait until the DOM is rendered before we initialise Vue
 document.addEventListener('DOMContentLoaded', () => {
