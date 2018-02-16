c8cc14e5a623b5f23818804f819c6d819afd2e4a
Ian Dickinson <i.j.dickinson@gmail.com>
2018-02-16 09:13:00 +0000

    More prod debugging

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,12 +14,16 @@ import SamplingPointMap from '../components/sampling-point-map.vue';
 
 // wait until the DOM is rendered before we initialise Vue
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('In domContentLoaded event handler', process.env.NODE_ENV === 'production');
-  debugger;
-  /* eslint-disable no-new */
-  new Vue({
-    el: '#map-container',
-    template: '<SamplingPointMap/>',
-    components: { SamplingPointMap },
-  });
+  try {
+    console.log('In domContentLoaded event handler.', process.env.NODE_ENV === 'production');
+    debugger;
+    /* eslint-disable no-new */
+    new Vue({
+      el: '#map-container',
+      template: '<SamplingPointMap/>',
+      components: { SamplingPointMap },
+    });
+  } catch (error) {
+    console.log('caught', error);
+  }
 });
